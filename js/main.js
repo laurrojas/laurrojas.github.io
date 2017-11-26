abae3a1dc49c5c797846df17cf53ad2c94161188
laurrojas <laurrojas@gmail.com>
2017-11-26 12:33:37 -0500

    File Upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$('.hover1').hover(function() {
+    $('.hoverimg1').toggleClass ('hideimg')
+    
+})
+
+$('.hover2').hover(function() {
+    $('.hoverimg2').toggleClass ('hideimg')
+    
+})
+
+$('.hover3').hover(function() {
+    $('.hoverimg3').toggleClass ('hideimg')
+    
+})
+
+
+
+
+$("#img2").on({
+ "mouseover" : function() {
+    this.src = 'img/line2.png';
+  },
+  "mouseout" : function() {
+    this.src='img/line.png';
+  }
+});
+
